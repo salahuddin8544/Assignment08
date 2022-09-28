44684969ac5315934bc9c03d101184f30220eee6
salahuddin8544 <salahuddinahmed8544@gmail.com>
2022-09-28 23:56:51 +0600

    fontaweseome added

--- src/Components/Kungfu/Kungfu.js
+++ src/Components/Kungfu/Kungfu.js
@@ -1,19 +1,32 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import React, { useEffect, useState } from 'react';
 import KungfuPlayer from '../KungfuPlayer/KungfuPlayer';
 import Profile from '../Profile/Profile';
 import('./Kungfu.css')
 const Kungfu = () => {
-    const [player,setPlayer]= useState([])
+    const [players,setPlayer]= useState([])
     useEffect(()=>{
         fetch('Kungfu.json')
         .then(res=>res.json())
         .then(data=>setPlayer(data))
     },[])
-    console.log(player);
     return (
         <div className='kunfu-container'>
+            <div>
+                <div className='kunfu-titla'>
+                    <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
+                    <h2>Kungfu master</h2>
+                </div>
             <div className='kunfu-player-container'>
-            <KungfuPlayer></KungfuPlayer>
+                
+                {
+                    players.map(player=><KungfuPlayer 
+                    key={player.id}
+                    player={player}
+                    ></KungfuPlayer>)
+                }
+                </div>
             </div>
             <div className='profile-container'>
             <Profile></Profile>

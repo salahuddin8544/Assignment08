94bac7c3a13e063dc44de85872e4051106e2eea5
salahuddin8544 <salahuddinahmed8544@gmail.com>
2022-09-29 09:11:23 +0600

    complete cart

--- src/Components/KungfuPlayer/KungfuPlayer.js
+++ src/Components/KungfuPlayer/KungfuPlayer.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ('./KungfuPlayer.css')
-const KungfuPlayer = () => {
+const KungfuPlayer = (props) => {
+    const{name,about,age,picture,time} = props.player
+    console.log(about);
     return (
-        <div>
-            <h2>This is player</h2>
+        <div className='cart'>
+            <img className='cover-img' src={picture} alt="" />
+            <div className='infp'>
+            <h3>{name}</h3>
+            <p> {about}</p>
+            <h4>For Age: {age}</h4>
+            <h4>Time required: {time}</h4>
+            </div>
+            <button>Add to List</button>
         </div>
     );
 };
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import('./Profile.css')
+import'./Profile.css'
+import {logo} from '../../logo.svg'
 const Profile = () => {
     return (
         <div>
-            <h4>this is form forofile</h4>
+            <div className='personal'>
+                {/* <img src={logo} alt="" /> */}
+                <h4>thi si img</h4>
+                <div>
+                    <h3>Salah uddin</h3>
+                    <p>Dhaka, Bangladesh</p>
+                </div>
+            </div>
         </div>
     );
 };
